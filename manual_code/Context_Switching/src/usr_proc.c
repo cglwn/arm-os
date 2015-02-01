5945d7d6185cab4cbbe88232c51347bc2e3b167b
unknown <ajcagala@ece-rtos5.NEXUS.UWATERLOO.CA>
2015-01-31 20:45:05 -0500

    4/6 processors

--- manual_code/Context_Switching/src/usr_proc.c
+++ manual_code/Context_Switching/src/usr_proc.c
@@ -66,10 +66,10 @@ void set_test_procs() {
 	g_test_procs[1].m_priority   = HIGH;
 	
 	g_test_procs[2].mpf_start_pc = &proc3;
-	g_test_procs[2].m_priority   = LOW;
+	g_test_procs[2].m_priority   = MEDIUM;
 	
 	g_test_procs[3].mpf_start_pc = &proc4;
-	g_test_procs[3].m_priority   = LOW;
+	g_test_procs[3].m_priority   = MEDIUM;
 	
 	g_test_procs[4].mpf_start_pc = &proc5;
 	g_test_procs[4].m_priority   = LOW;
@@ -79,6 +79,12 @@ void set_test_procs() {
 	uart0_put_string("\n\r\n\r");
 }
 
+
+void null_proc(void) { 
+	while(1) {
+		release_processor();
+	}
+}
 
 /**
  * @brief: a process that prints five uppercase letters
@@ -138,27 +144,59 @@ void proc2(void)
 
 void proc3(void)
 {
-	int i=0;
-	
-	while(1) {
-		if ( i < 2 ) {
-			uart0_put_string("proc3: \n\r");
+	int i = 0;
+	int counter = 0;
+	int ret_val = 100;
+	while ( 1 ) {
+		
+		if ( i != 0 && i%5 == 0 ) {
+			uart0_put_string("\n\r");
+			counter++;
+			if ( counter == 2 ) {
+				ret_val = set_process_priority(PID_P4, HIGH);
+				break;
+			} else {
+				ret_val = release_processor();
+			}
+#ifdef DEBUG_0
+			printf("proc3: ret_val = %d \n", ret_val);
+#endif /* DEBUG_0 */
 		}
-		release_processor();
+		uart0_put_char('a' + i%10);
 		i++;
+	}
+	uart0_put_string("proc3 end of testing\n\r");
+	while ( 1 ) {
+		release_processor();
 	}
 }
 
 void proc4(void)
 {
-	int i=0;
+	int i = 0;
+	int ret_val = 20;
+	int counter = 0;
 	
-	while(1) {
-		if ( i < 2 ) {
-			uart0_put_string("proc4: \n\r");
+	while ( 1) {
+		if ( i != 0 && i%5 == 0 ) {
+			uart0_put_string("\n\r");
+			counter++;
+			if ( counter == 4 ) {
+				ret_val = set_process_priority(PID_P3, HIGH);
+				break;
+			} else {
+				ret_val = release_processor();
+			}
+#ifdef DEBUG_0
+			printf("proc4: ret_val=%d\n", ret_val);
+#endif /* DEBUG_0 */
 		}
-		release_processor();
+		uart0_put_char('0' + i%10);
 		i++;
+	}
+	uart0_put_string("proc4 end of testing\n\r");
+	while ( 1 ) {
+		release_processor();
 	}
 }
 void proc5(void)
