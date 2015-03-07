66e6f9114395398c24c91b91ca130af65a8fb851
James Cagalawan <ajcagala@uwaterloo.ca>
2015-03-07 03:17:49 -0500

    Removed a bunch of extraneous object files

--- manual_code/Context_Switching/k_utilities.d
+++
@@ -1,7 +0,0 @@
-k_utilities.o: src\k_utilities.c
-k_utilities.o: src\k_utilities.h
-k_utilities.o: src\k_rtx.h
-k_utilities.o: src\k_memory.h
-k_utilities.o: C:\Software\Keil\ARM\ARMCC\bin\..\include\assert.h
-k_utilities.o: src\printf.h
-k_utilities.o: C:\Software\Keil\ARM\ARMCC\bin\..\include\stdarg.h
